12c5b6a5e63bda8e2493c8b1d722453bd5a79c4c
zhekix <zhekix@gmail.com>
2018-03-05 10:21:28 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,26 @@
+module.exports = function longestConsecutiveLength(array) {
+  if (array.length == 0) return 0;
+  if (array.length == 1) return 1;
+
+  var arr = [].concat(array);
+  arr.sort(compareNumeric);
+
+  var max   = 0;
+  var count = 1;
+  arr.map(function(item,index, array){
+    var diff = item - array[index-1];
+    if ( diff == 1 ) count++;
+    if ( diff > 1 ) {
+      max = count > max ? count : max;
+      count = 1;
+    }
+  });
+
+  return max;
+
+}
+
+function compareNumeric(a,b) {
+  if (a > b) return 1;
+  if (a < b) return -1;
+}
